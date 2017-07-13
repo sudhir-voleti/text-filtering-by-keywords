61b1fa30000e96c443776db5edbb60f115612fad
Sudhir Voleti <sudhir_voleti@isb.edu>
2017-07-13 11:08:43 +0530

    Update ui.R

--- ui.R
+++ ui.R
@@ -27,27 +27,39 @@ shinyUI(fluidPage(
   mainPanel( 
         tabsetPanel(type = "tabs",
                 #
-                tabPanel("Overview",h4(p("How to use this App")),
+                tabPanel("Overview", h4(p("How to use this App")),
                          
-                         p("To use this app you need a document corpus in txt file format. Make sure each document is 
-                           separated from another document with a new line character. Similarly you will need a keywords 
-                           txt file. Each Key Word should be separated by a new line. You can download the sample files 
-                           from Example dataset tab. Also you can enter the keywords in left side bar panel. If you are 
+                         p("Imagine you have a list of keywords, say 'customer', 'brand' etc., which you want to search every instance of in a corpus. And not just that, you want to also collect some context around each keyword's occurence by examining a few words on either side of the keyword. Now you can use Ctrl + F and lookup the words but if you want these context laden text chunks all inone place, then this app can help.", align = "justify")
+                         
+                         p("To use this app you'll need a few things: [1] A document corpus to search in, in .txt file format. Make sure each document is 
+                           separated from another document with a new line character. [2] A keywords list again as a .txt file. Each Key Word should be separated by a new line. 
+                           [3] Alternately, enter oen or more keywords separated by commas in the second input textbox (in left side bar panel).", align = "justify")
+    
+                          p(" Note: You can download the sample files from the 'Example dataset' tab and see how the app works. Also you can enter the keywords in left side bar panel. If you are 
                            entering keywords in the left side bar panel, than each key word should be separated by comma (,) 
                            without any space.", align = "justify")
                          )
                 ,
-                tabPanel("Example dataset", h4(p("Download Sample text file")), 
-                         downloadButton('downloadData1', 'Download Nokia Lumia reviews text file'),br(),
+    
+                tabPanel("Example dataset", 
+                         
+                         h4(p("Download Sample text file")), 
+                         downloadButton('downloadData1', 'Download Nokia Lumia reviews text file'),
+                         br(),
+                         
                          h4(p("Download Sample Key Words text file")), 
                          downloadButton('downloadData2', 'Download Sample Key Words text file'),br(),
                          br(),
+                         
                          p("Please note that download will not work with RStudio interface. Download will work only in web-browsers. So open this app in a web-browser and then download the example file. For opening this app in web-browser click on \"Open in Browser\" as shown below -"),
                          img(src = "example1.png")),
-                
+    
+    
                 tabPanel("Filtered Corpus",
                          h4(p("Download Filtered Corpus text file")), 
-                         downloadButton('downloadData3', 'Download Filtered Corpus text file'),br(),br(),
+                         downloadButton('downloadData3', 'Download Filtered Corpus text file'),
+                         br(),
+                         br(),
                          verbatimTextOutput("filter_corp"))
                 )
            )
