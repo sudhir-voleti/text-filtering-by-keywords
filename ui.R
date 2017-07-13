6c9c8fd66cd1f6711e97b8a0ab2975c61cc765d9
Sudhir Voleti <sudhir_voleti@isb.edu>
2017-07-13 11:14:50 +0530

    Update ui.R

--- ui.R
+++ ui.R
@@ -27,14 +27,22 @@ shinyUI(fluidPage(
   mainPanel( 
         tabsetPanel(type = "tabs",
                 #
-                tabPanel("Overview", h4(p("How to use this App")),
+                tabPanel("Overview", 
+                         h4(p("Why use this App")),
+                         p("Suppose you have a list of keywords, say 'customer', 'brand' etc., 
+                             which you want to search every instance of, in some corpus (say, a consumer insights report). 
+                             Furthermore, you want some context around each keyword's occurence - say 20 odd words before and after each keyword. 
+                             Now one can use Ctrl + F directly on the corpus. 
+                             But if you want these context laden text chunks all neatly in one place, then this app can help.", align = "justify"),
                          
-                         p("Imagine you have a list of keywords, say 'customer', 'brand' etc., which you want to search every instance of in a corpus. And not just that, you want to also collect some context around each keyword's occurence by examining a few words on either side of the keyword. Now you can use Ctrl + F and lookup the words but if you want these context laden text chunks all inone place, then this app can help.", align = "justify"),
+                         h4(p("How to use this App")),
                          
                          p("To use this app you'll need a few things: [1] A document corpus to search in, in .txt file format. Make sure each document is 
                            separated from another document with a new line character. [2] A keywords list again as a .txt file. Each Key Word should be separated by a new line. 
                            [3] Alternately, enter oen or more keywords separated by commas in the second input textbox (in left side bar panel).", align = "justify"),
     
+                          h4(p("Test Drive the App with the example dataset")),
+        
                           p(" Note: You can download the sample files from the 'Example dataset' tab and see how the app works. Also you can enter the keywords in left side bar panel. If you are 
                            entering keywords in the left side bar panel, than each key word should be separated by comma (,) 
                            without any space.", align = "justify")
